470145b08bea570007a334e9b43c924857abbaa5
yingnierxiao@qq.com <yingnierxiao@qq.com>
2014-09-15 14:46:30 +0800

    del openvc

--- opencv/modules/java/test/CMakeFiles/opencv_test_java_properties.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/opencv_test_java_properties"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/opencv_test_java_properties.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
