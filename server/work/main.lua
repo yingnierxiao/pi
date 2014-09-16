32cbfe3213d7c15aeb8c8bf930209609351f98bc
yingnierxiao@qq.com <yingnierxiao@qq.com>
2014-09-16 11:13:14 +0800

    change dir ok

--- server/work/main.lua
+++ server/work/main.lua
@@ -1,9 +1,8 @@
 local skynet = require "skynet"
-
+local snax = require "snax"
 local max_client = 64
 
 skynet.start(function()
-	print("Server start")
 	local watchdog = skynet.newservice("watchdog")
 	skynet.call(watchdog, "lua", "start", {
 		port = 8888,
@@ -11,5 +10,6 @@ skynet.start(function()
 		nodelay = true,
 	})
 	print("Watchdog listen on ", 8888)
+	local car = snax.uniqueservice("carServer")
 	skynet.exit()
 end)
